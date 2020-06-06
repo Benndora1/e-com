866305dc16439e53da4f6e54d40d0c29ac90f41c
benndora2 <benndora2@gmail.com>
2020-06-06 23:38:12 +0300

    first commit

---
+++ shop/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+# Create your views here.
+def product_list(request, category_slug=None):
+    category =None
+    categories = category.object.all()
+    products = product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = product.filter(category=category)
+    return render(request,
+                 'shop/product/list.html',
+                 {'category': category,
+                 'categories: categories'
+                 'products: products'})
+
+def product_details(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=true)
+    
+    return render(request,
+                  'shop/product/details.html',
+                  {'product': product})
